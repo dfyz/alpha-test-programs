74d01be2b2299a0ca0ecc55b6fe77586d1b46a59
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-08-07 04:49:41 +0300

    Give up and use the built-in ld from Ubuntu instead of trying to force clang to find all required files

--- config.mk
+++ config.mk
@@ -3,17 +3,18 @@ OBJECTS=$(SOURCES:.c=.o)
 CC=~/build/bin/clang
 WARN_OPTS=-Wall -pedantic
 FLAGS=-O2 -std=c11 -g
-PLATFORM_FLAGS=--target=alpha-linux-gnu --sysroot=/usr/alpha-linux-gnu/
+PLATFORM_FLAGS=--target=alpha-linux-gnu --sysroot=/usr/alpha-linux-gnu
 LLVM_FLAGS=
-LINKER_FLAGS=
 CFLAGS=$(WARN_OPTS) $(FLAGS) $(PLATFORM_FLAGS) $(LLVM_FLAGS)
+
+LD=alpha-linux-gnu-gcc-9
 
 %.o: %.c
 	$(CC) -c $(CFLAGS) $< -o $@
 
 .PHONY: main
 main: $(OBJECTS)
-	$(CC) $(PLATFORM_FLAGS) $(LINKER_FLAGS) -o main $(OBJECTS)
+	$(LD) -o main $(OBJECTS) -lm
 
 .PHONY: clean
 clean:
