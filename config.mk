e64558e55697e02973d9e30ba707251e6efd7b34
Ivan.Komarov <Ivan.Komarov@dfyz.info>
2020-06-01 02:01:57 +0300

    Bump GCC version

--- config.mk
+++ config.mk
@@ -1,13 +1,13 @@
 OBJECTS=$(SOURCES:.c=.o)
 
-CC=~/build/bin/clang
+CC=~/llvm-build/bin/clang
 WARN_OPTS=-Wall -pedantic
 FLAGS=-O2 -std=c11 -g
 PLATFORM_FLAGS=--target=alpha-linux-gnu --sysroot=/usr/alpha-linux-gnu
 LLVM_FLAGS=
 CFLAGS=$(WARN_OPTS) $(FLAGS) $(PLATFORM_FLAGS) $(LLVM_FLAGS)
 
-LD=alpha-linux-gnu-gcc-9
+LD=alpha-linux-gnu-gcc-10
 
 %.o: %.c
 	$(CC) -c $(CFLAGS) $< -o $@
